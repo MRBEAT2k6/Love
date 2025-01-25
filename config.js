4f0ddda7c007a0474a0fb6234f03ef6e463aa4cc
Meo23 <86160493+MRBEAT2k6@users.noreply.github.com>
2025-01-25 21:25:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Vương Thị Mai',
+    introTitle: 'Tam Tran',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
